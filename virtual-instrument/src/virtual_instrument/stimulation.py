1772ba350efb8c5f642c46075871a4eec5fd2dbd
Tanner Peterson <tannermp23@hotmail.com>
2023-03-24 21:12:31 -0700

    clean up + share code between controller and VI

--- virtual-instrument/src/virtual_instrument/stimulation.py
+++ virtual-instrument/src/virtual_instrument/stimulation.py
@@ -3,120 +3,11 @@
 
 import copy
 from typing import Any
-from typing import Dict
-from typing import List
-from typing import Optional
-from typing import Tuple
-from typing import Union
-
-from .constants import STIM_MAX_CHUNKED_SUBPROTOCOL_DUR_MICROSECONDS
-from .constants import STIM_MIN_SUBPROTOCOL_DURATION_MICROSECONDS
-
-SUBPROTOCOL_DUTY_CYCLE_DUR_COMPONENTS = frozenset(
-    ["phase_one_duration", "interphase_interval", "phase_two_duration"]
-)
-
-
-def get_pulse_duty_cycle_dur_us(subprotocol: Dict[str, Union[str, int]]) -> int:
-    return sum(subprotocol.get(comp, 0) for comp in SUBPROTOCOL_DUTY_CYCLE_DUR_COMPONENTS)  # type: ignore
-
-
-def get_pulse_dur_us(subprotocol: Dict[str, Union[str, int]]) -> int:
-    return get_pulse_duty_cycle_dur_us(subprotocol) + subprotocol["postphase_interval"]  # type: ignore
-
-
-def get_subprotocol_dur_us(subprotocol: Dict[str, Union[str, int]]) -> int:
-    duration = (
-        subprotocol["duration"]
-        if subprotocol["type"] == "delay"
-        else get_pulse_dur_us(subprotocol) * subprotocol["num_cycles"]
-    )
-    return duration  # type: ignore
-
-
-def chunk_subprotocol(
-    original_subprotocol: Dict[str, Any]
-) -> Tuple[Optional[Dict[str, Any]], Optional[Dict[str, Any]]]:
-    # copy so the original subprotocol dict isn't modified
-    original_subprotocol = copy.deepcopy(original_subprotocol)
-    original_subprotocol_dur_us = get_subprotocol_dur_us(original_subprotocol)
-
-    if (
-        original_subprotocol["type"] == "delay"
-        or original_subprotocol_dur_us <= STIM_MAX_CHUNKED_SUBPROTOCOL_DUR_MICROSECONDS
-    ):
-        return None, original_subprotocol
-
-    original_num_cycles = original_subprotocol["num_cycles"]
-
-    pulse_dur_us = get_pulse_dur_us(original_subprotocol)
-    max_num_cycles_in_loop = STIM_MAX_CHUNKED_SUBPROTOCOL_DUR_MICROSECONDS // pulse_dur_us
-    looped_subprotocol = {**original_subprotocol, "num_cycles": max_num_cycles_in_loop}
-    num_loop_iterations = original_subprotocol_dur_us // get_subprotocol_dur_us(looped_subprotocol)  # type: ignore
-
-    loop_chunk = {"type": "loop", "num_iterations": num_loop_iterations, "subprotocols": [looped_subprotocol]}
-
-    leftover_chunk = None
-
-    if num_leftover_cycles := original_num_cycles - (looped_subprotocol["num_cycles"] * num_loop_iterations):
-        leftover_chunk = {**original_subprotocol, "num_cycles": num_leftover_cycles}
-
-        if get_subprotocol_dur_us(leftover_chunk) < STIM_MIN_SUBPROTOCOL_DURATION_MICROSECONDS:
-            # if there is only 1 loop iteration, then just return the original subprotocol
-            if loop_chunk["num_iterations"] == 1:
-                return None, original_subprotocol
-            # otherwise, combine leftover chunk with the final loop iteration
-            leftover_chunk["num_cycles"] += looped_subprotocol["num_cycles"]
-            loop_chunk["num_iterations"] -= 1  # type: ignore
-
-    return loop_chunk, leftover_chunk
-
-
-def chunk_protocols_in_stim_info(
-    stim_info: Dict[str, Any]
-) -> Tuple[Dict[str, Any], Dict[str, Dict[int, int]], Dict[str, Tuple[int, ...]]]:
-    # copying so the original dict passed in does not get modified
-    chunked_stim_info = copy.deepcopy(stim_info)
-
-    subprotocol_idx_mappings = {}
-    max_subprotocol_idx_counts = {}
-
-    for protocol in chunked_stim_info["protocols"]:
-        curr_chunked_idx = 0
-
-        chunked_idx_to_original_idx = {}
-        original_idx_counts = []
-        new_subprotocols = []
-
-        for original_idx, subprotocol in enumerate(protocol["subprotocols"]):
-            original_idx_count = 0
-
-            for chunk in chunk_subprotocol(subprotocol):
-                if not chunk:
-                    continue
-
-                chunked_idx_to_original_idx[curr_chunked_idx] = original_idx
-                # for loop chunk, need to count the num iterations, leftover chunk is always 1
-                original_idx_count += chunk.get("num_iterations", 1)
-                new_subprotocols.append(chunk)
-
-                curr_chunked_idx += 1
-
-            original_idx_counts.append(original_idx_count)
-
-        # FW requires top level to be a single loop
-        protocol["subprotocols"] = [{"type": "loop", "num_iterations": 1, "subprotocols": new_subprotocols}]
-
-        protocol_id = protocol["protocol_id"]
-        subprotocol_idx_mappings[protocol_id] = chunked_idx_to_original_idx
-        max_subprotocol_idx_counts[protocol_id] = tuple(original_idx_counts)
-
-    return chunked_stim_info, subprotocol_idx_mappings, max_subprotocol_idx_counts
 
 
 class StimulationProtocolManager:
     def __init__(
-        self, subprotocols: List[Dict[str, Any]], num_iterations: Optional[int] = None, start_idx: int = 0
+        self, subprotocols: list[dict[str, Any]], num_iterations: int | None = None, start_idx: int = 0
     ) -> None:
         if num_iterations is not None and num_iterations < 1:  # pragma: no cover
             raise ValueError("num_iterations must be >= 1")
@@ -127,8 +18,8 @@ class StimulationProtocolManager:
 
         self._subprotocol_idx: int
         self._node_idx: int
-        self._num_iterations_remaining: Optional[int]
-        self._loop: Optional[StimulationProtocolManager]
+        self._num_iterations_remaining: int | None
+        self._loop: StimulationProtocolManager | None
         self._reset()
 
     def _reset(self) -> None:
@@ -150,7 +41,7 @@ class StimulationProtocolManager:
         # if on the final node and no loop is present, then the protocol is complete
         return True
 
-    def current(self) -> Dict[str, Any]:
+    def current(self) -> dict[str, Any]:
         # if there is a loop, the current subprotocol must be retrieved from it the current node at this level is a loop
         if self._loop:
             return self._loop.current()
@@ -165,7 +56,7 @@ class StimulationProtocolManager:
         # otherwise, return the current idx stored at this level
         return self._subprotocol_idx
 
-    def advance(self) -> Dict[str, Any]:
+    def advance(self) -> dict[str, Any]:
         # if there is a loop, advance to the next subprotocol node within the loop first
         if self._loop:
             try:
