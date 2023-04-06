e183a1bf0ee8f05586920655813ea600cd7910f1
Luci Pak <84344924+luciipak@users.noreply.github.com>
2023-04-06 11:06:59 +1000

    Error modal styling (#12)

---
+++ ui/pages/status/error-catch-basic.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ErrorCatchWidget logFilepath="C:\Users\Mantarray\AppData\Roaming\MantarrayController\stingray_logs" />
+  </div>
+</template>
+
+<script>
+// import { ErrorCatchWidget as ComponentToTest } from "@/dist/mantarray.common";
+import ErrorCatchWidget from "@/components/status/ErrorCatchWidget.vue";
+
+export default {
+  components: {
+    ErrorCatchWidget,
+  },
+};
+</script>
