a24f9b52a6f98aceb061e15e2e7e2ac01462008b
Tanner Peterson <tannermp23@hotmail.com>
2023-04-12 22:44:21 -0700

    fix VI metadata

--- virtual-instrument/src/virtual_instrument/virtual_instrument.py
+++ virtual-instrument/src/virtual_instrument/virtual_instrument.py
@@ -118,9 +118,9 @@ class MantarrayMcSimulator(InfiniteProcess):
     )
 
     default_mantarray_nickname = "Vrtl Stingray"
-    default_mantarray_serial_number = "M02022140001"
-    default_main_firmware_version = "1.1.6"
-    default_channel_firmware_version = "1.1.2"
+    default_mantarray_serial_number = "MA2023102001"
+    default_main_firmware_version = "0.0.0"
+    default_channel_firmware_version = "0.0.0"
     default_plate_barcode = "ML22001000-2"
     default_stim_barcode = "MS22001000-2"
     default_metadata_values: immutabledict[UUID, Any] = immutabledict(
